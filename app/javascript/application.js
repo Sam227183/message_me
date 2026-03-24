1fdb452f375b958e8e817b9e77f716be5ea040a5
Hansaem Kim <dalbyul9062@gmail.com>
2026-03-24 17:50:30 -0600

    dropdown initialization on DOMContentLoaded and turbo:load events

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// app/javascript/application.js
+
+window.addEventListener('DOMContentLoaded', function() {
+  if (window.$) {
+    $('.ui.dropdown').dropdown();
+  }
+});
+document.addEventListener('turbo:load', function() {
+  if (window.$) {
+    $('.ui.dropdown').dropdown();
+  }
+});
